f0ad92d42a6cf93bdde0df311cdcdd22f9c6bf01
Wang Hao <237466265@qq.com>
2022-12-23 12:43:21 +0800

    homework 1 finished

---
+++ Homework1/Assignment1/main.cpp
@@ -0,0 +1,144 @@
+#include "Triangle.hpp"
+#include "rasterizer.hpp"
+#include <eigen3/Eigen/Eigen>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+constexpr double PI = 3.1415926;
+
+Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
+{
+    Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
+
+    Eigen::Matrix4f translate;
+    translate << 1, 0, 0, -eye_pos[0], 0, 1, 0, -eye_pos[1], 0, 0, 1,
+        -eye_pos[2], 0, 0, 0, 1;
+
+    view = translate * view;
+
+    return view;
+}
+
+Eigen::Matrix4f get_model_matrix(float rotation_angle)
+{
+    Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
+
+    // TODO: Implement this function
+    // Create the model matrix for rotating the triangle around the Z axis.
+    // Then return it.
+    model << cos(rotation_angle / 180 * PI), -sin(rotation_angle / 180 * PI), 0, 0,
+            sin(rotation_angle / 180 * PI), cos(rotation_angle / 180 * PI), 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1;
+
+    return model;
+}
+
+Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
+                                      float zNear, float zFar)
+{
+    // 按顺序乘以这些矩阵得到projection
+    Eigen::Matrix4f persp_to_ortho = Eigen::Matrix4f::Identity();
+    Eigen::Matrix4f translation = Eigen::Matrix4f::Identity();
+    Eigen::Matrix4f scale = Eigen::Matrix4f::Identity();
+
+    Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
+
+    // TODO: Implement this function
+    // Create the projection matrix for the given parameters.
+    // Then return it.
+
+    float n, f, H, W;
+    n = zNear;
+    f = zFar;
+    H = -zNear * tan(eye_fov / 180 * PI / 2);
+    W = H * aspect_ratio;
+
+    persp_to_ortho << n, 0, 0, 0,
+                    0, n, 0, 0, 
+                    0, 0, n+f, -n*f,
+                    0, 0, 1, 0;
+
+    translation << 1, 0, 0, 0,
+                    0, 1, 0, 0,
+                    0, 0, 1, -(f+n)/2,
+                    0, 0, 0, 1;
+        
+    scale << 1/W, 0, 0, 0, // [-W, W]*[-H, H]*[f, n] to [-1, 1]*[-1, 1]*[-1, 1]
+            0, 1/H, 0, 0,
+            0, 0, 2/(n-f), 0,
+            0, 0, 0, 1;
+    projection = scale * translation * persp_to_ortho;
+    return projection;
+}
+
+int main(int argc, const char** argv)
+{
+    float angle = 0;
+    bool command_line = false;
+    std::string filename = "output.png";
+
+    if (argc >= 3) {
+        command_line = true;
+        angle = std::stof(argv[2]); // -r by default
+        if (argc == 4) {
+            filename = std::string(argv[3]);
+        }
+    }
+
+    rst::rasterizer r(700, 700);
+
+    Eigen::Vector3f eye_pos = {0, 0, 5};
+
+    std::vector<Eigen::Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}};
+
+    std::vector<Eigen::Vector3i> ind{{0, 1, 2}};
+
+    auto pos_id = r.load_positions(pos);
+    auto ind_id = r.load_indices(ind);
+
+    int key = 0;
+    int frame_count = 0;
+
+    if (command_line) {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        r.set_model(get_model_matrix(angle));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+
+        cv::imwrite(filename, image);
+
+        return 0;
+    }
+
+    while (key != 27) {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        r.set_model(get_model_matrix(angle));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+        cv::imshow("image", image);
+        key = cv::waitKey(10);
+
+        std::cout << "frame count: " << frame_count++ << '\n';
+
+        if (key == 'a') {
+            angle += 10;
+        }
+        else if (key == 'd') {
+            angle -= 10;
+        }
+    }
+
+    return 0;
+}
---
+++ Homework1/Assignment1/rasterizer.hpp
@@ -0,0 +1,98 @@
+//
+// Created by goksu on 4/6/19.
+//
+
+#pragma once
+
+#include "Triangle.hpp"
+#include <algorithm>
+#include <eigen3/Eigen/Eigen>
+using namespace Eigen;
+
+namespace rst {
+enum class Buffers
+{
+    Color = 1,
+    Depth = 2
+};
+
+inline Buffers operator|(Buffers a, Buffers b)
+{
+    return Buffers((int)a | (int)b);
+}
+
+inline Buffers operator&(Buffers a, Buffers b)
+{
+    return Buffers((int)a & (int)b);
+}
+
+enum class Primitive
+{
+    Line,
+    Triangle
+};
+
+/*
+ * For the curious : The draw function takes two buffer id's as its arguments.
+ * These two structs make sure that if you mix up with their orders, the
+ * compiler won't compile it. Aka : Type safety
+ * */
+struct pos_buf_id
+{
+    int pos_id = 0;
+};
+
+struct ind_buf_id
+{
+    int ind_id = 0;
+};
+
+class rasterizer
+{
+  public:
+    rasterizer(int w, int h);
+    pos_buf_id load_positions(const std::vector<Eigen::Vector3f>& positions);
+    ind_buf_id load_indices(const std::vector<Eigen::Vector3i>& indices);
+
+    // 将内部的模型矩阵作为参数传递给光栅化器。
+    void set_model(const Eigen::Matrix4f& m);
+
+    // 将视图变换矩阵设为内部视图矩阵。
+    void set_view(const Eigen::Matrix4f& v);
+
+    // 将内部的投影矩阵设为给定矩阵 p，并传递给光栅化器
+    void set_projection(const Eigen::Matrix4f& p);
+    
+    // 将屏幕像素点 (x, y) 设为 (r, g, b) 的颜色，并写入相应的帧缓冲区位置。
+    void set_pixel(const Eigen::Vector3f& point, const Eigen::Vector3f& color);
+
+    void clear(Buffers buff);
+
+    void draw(pos_buf_id pos_buffer, ind_buf_id ind_buffer, Primitive type);
+
+    std::vector<Eigen::Vector3f>& frame_buffer() { return frame_buf; }
+
+  private:
+    void draw_line(Eigen::Vector3f begin, Eigen::Vector3f end);
+    void rasterize_wireframe(const Triangle& t);
+
+  private:
+    // 三个变换矩阵
+    Eigen::Matrix4f model;
+    Eigen::Matrix4f view;
+    Eigen::Matrix4f projection;
+
+    std::map<int, std::vector<Eigen::Vector3f>> pos_buf;
+    std::map<int, std::vector<Eigen::Vector3i>> ind_buf;
+
+    // 帧缓冲对象，用于存储需要在屏幕上绘制的颜色数据。
+    std::vector<Eigen::Vector3f> frame_buf;
+    std::vector<float> depth_buf;
+    int get_index(int x, int y);
+
+    int width, height;
+
+    int next_id = 0;
+    int get_next_id() { return next_id++; }
+};
+} // namespace rst
