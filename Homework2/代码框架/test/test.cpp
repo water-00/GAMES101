084b64f2e4bf56a95ebc606a5f52966c80c0e544
Wang Hao <237466265@qq.com>
2022-12-31 18:05:01 +0800

    homework 2 finished

--- Homework2/代码框架/test/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <eigen3/Eigen/Eigen>
-#include <iostream>
-
-int main() {
-    Eigen::Matrix<float, 4, 1> s(1.0, 2.0, 3.0, 4.0);
-    std::cout << s << std::endl;
-    for (int i = 0; i < s.size(); i++)
-        std::cout << s[i] << std::endl;
-}
